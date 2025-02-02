88c965433cebd740788c6356ec79ccb56557f42c
someshmewal <111623264+someshmewal@users.noreply.github.com>
2025-02-02 22:11:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+import typeDefs from './schema/schema.js';
+import resolvers from './resolvers/resolver.js';
+
+const app = express();
+
+// Create Apollo Server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = async () => {
+  await server.start();
+
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  app.use("/graphql", expressMiddleware(server));
+
+
+  app.listen(4000, () => {
+    console.log('Server is running at http://localhost:4000/graphql');
+  });
+};
+
+startServer();
